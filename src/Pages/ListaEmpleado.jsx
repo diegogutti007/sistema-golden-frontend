7736edb7536cd114f0046ef7b912d0209744346d
Your Name <D525@WIENERGROUP.COM>
2025-12-03 11:31:14 -0500

    mensaje

--- src/Pages/ListaEmpleado.jsx
+++ src/Pages/ListaEmpleado.jsx
@@ -44,8 +44,8 @@ function ListaEmpleado() {
 
   useEffect(() => {
     // Para Create React App usa REACT_APP_API_URL
-    const url = "https://sistemagolden-backend-production.up.railway.app"//process.env.REACT_APP_API_URL || "http://localhost:5000"//"https://sistemagolden-backend-production.up.railway.app";//
-    
+    const url = process.env.REACT_APP_API_URL || "http://localhost:5000"//"https://sistemagolden-backend-production.up.railway.app";//
+    //"https://sistemagolden-backend-production.up.railway.app"
     setBackendUrl(url);
     console.log("🔗 URL del backend detectada:", url);
   }, []);
